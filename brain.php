81d49c0b1a812c6ae309ad4df117f23ce96222b6
bhowmikp <prantarbhowmik@yahoo.com>
2016-08-17 20:54:14 -0400

    Add persoanlity by telling random facts

--- brain.php
+++ brain.php
@@ -1,7 +1,7 @@
 <?php
 // finds the keywords nad makes message smaller
 function make_smaller($message){
-	$KEYWORDS = ['when', 'where', 'how', 'what', 'name', 'club', 'csec'];
+	$KEYWORDS = ['when', 'where', 'how', 'what', 'name', 'club', 'csec', 'random', 'trivia', 'year', 'date', 'math'];
 	$message = explode(" ", $message);
 	
 	foreach ($message as &$word){
@@ -15,6 +15,21 @@ function make_smaller($message){
 function check_cases($message){
 	if ($message == 'what name club'){
 		return 'The name of the club is Computer Science Enrichment Club (CSEC)';
+	}
+	elseif($message == 'random'){
+		return file_get_contents('http://numbersapi.com/random/');
+	}
+	elseif($message == 'random' || $message == 'random trivia' || $message == 'random fact'){
+		return file_get_contents('http://numbersapi.com/random/trivia');
+	}
+	elseif($message == 'random year fact' || $message == 'random year'){
+		return file_get_contents('http://numbersapi.com/random/year');
+	}
+	elseif($message == 'random date fact' || $message == 'random date'){
+		return file_get_contents('http://numbersapi.com/random/date');
+	}
+	elseif($message == 'random math fact' || $message == 'random math'){
+		return file_get_contents('http://numbersapi.com/random/math');
 	}
 }
 // Calls on the necessary functions to give the bot ability to think
--- fixedAnswer.php
+++ fixedAnswer.php
@@ -4,13 +4,13 @@ function hardcode($message){
 	if ($message == 'hello' || $message == 'hi' || $message == 'good morning' || $message == 'good evening'){
 		return 'Hi';
 	}
-	elseif ($message == 'what is name of the club' || strpos($message, 'name of club') || strpos($message, 'name of the club')){
+	elseif ($message == 'what is name of the club' || strpos($message, 'name of club') || strpos($message, 'name of the club') || $message == 'name of club' || $message == 'name of the club'){
 		return 'The name of the club is Computer Science Enrichment Club (CSEC)';
 	}
 	elseif ($message == 'good night' || $message == 'goodnight' || $message == 'good bye' || $message == 'goodbye' || $message == 'bye'){
 		return 'Goodbye. Have a nice day :)';
 	}
-	elseif (strpos($message, 'who is best') || strpos($message, 'who is the best')){
+	elseif ($message == 'who is best' || $message == 'who is the best'){
 		return 'Prantar is the best';
 	} 
 	else{
